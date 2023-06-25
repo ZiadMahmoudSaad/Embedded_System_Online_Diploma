eeada499f3473ab69808a422f5c7ee112fc4c5e8
ziad mahmoud <ziadmahmoud55500@gmail.com>
2023-06-25 23:59:27 +0300

    EX2 solution

---
+++ Unit 2/Lecture 3 assignment C basics/Homework 1/EX2/EX2.c
@@ -0,0 +1,17 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Jun 25, 2023
+ *      Author: Ziad Mahmoud Saad
+ */
+
+#include <stdio.h>
+
+void main()
+{
+	int x;
+	printf("Enter a integer: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered: %d",x);
+}
